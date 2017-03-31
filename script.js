6f34c06bd55c5491e1b971419f2c068482b36bc6
nipa1958 <paynter.nicholas@gmail.com>
2017-03-31 12:07:43 -0400

    add the second song, clean up code, add comments

--- script.js
+++ script.js
@@ -40,10 +40,19 @@ $(document).ready(function(){
 	});
 });
 
+/*
+The function playSong creates an audio element, changes the src attribute of said elemnt
+and also changes the src attribute of the main image on the page
+then plays the song file
+*/
 function playSong(note){
 	var song = document.createElement('audio');
-	song.setAttribute('src', 'audio/'+note);
+	//set src attribute to audio element as audio/"audioname".mp3
+	song.setAttribute('src', 'audio/'+note+".mp3");
+	//set image #panImage src to image/"audioname".gif
+	$("#panImage").attr("src","image/"+note+".gif");
 	song.play();
+	//play particular sound
 }
 
 $(document).ready(function(){
@@ -65,89 +74,83 @@ $(document).ready(function(){
 		$('[id^=HT]').parent().hide();
 		$('[id^=TB]').parent().show(); 
 	});
-	
-	/*$(songone).click(function(){					
-		setTimeout(function(){
-			playSong("htG5.mp3");
-		}, 0);
-		setTimeout(function(){
-			playSong("htC4.mp3");
-		}, 500);
-		setTimeout(function(){
-			playSong("htD4.mp3");
-		}, 1000);
-		setTimeout(function(){
-			playSong("htE4.mp3");
-		}, 1500);
-		setTimeout(function(){
-			playSong("htE4.mp3");
-		}, 3000);
-		setTimeout(function(){
-			playSong("htE4.mp3");
-		}, 4000);
-		setTimeout(function(){
-			playSong("htD4.mp3");
-		}, 4500);					
-		setTimeout(function(){
-			playSong("htE4.mp3");
-		}, 5000);
-		setTimeout(function(){
-			playSong("htC4.mp3");
-		}, 5500);
-		setTimeout(function(){
-			playSong("htC4.mp3");
-		}, 7000);				    
-	});*/
-	$(songone).click(function(){					
-		setTimeout(function(){
-			playSong("htG4.mp3");
-		}, 0);
-		setTimeout(function(){
-			playSong("htG4.mp3");
-		}, 500);
-		setTimeout(function(){
-			playSong("htA4.mp3");
-		}, 1000);
-		setTimeout(function(){
-			playSong("htG4.mp3");
-		}, 1500);
-		setTimeout(function(){
-			playSong("htC4.mp3");
-		}, 2000);
-		setTimeout(function(){
-			playSong("htB4.mp3");
-		}, 2500);
+
+	$(songone).click(function(){
+	// Happy Birthday Song
+	// timers were set to give the song a tru feel of how it actually sounds where 1000 = 1 second					
+		setTimeout(function(){playSong("htG4");}, 0);
+		setTimeout(function(){playSong("htG4");}, 500);
+		setTimeout(function(){playSong("htA4");}, 1000);
+		setTimeout(function(){playSong("htG4");}, 1500);
+		setTimeout(function(){playSong("htC4");}, 2000);
+		setTimeout(function(){playSong("htB4");}, 2500);
 		// pause
-			playSong("htG4.mp3");
-		}, 3500);
-		setTimeout(function(){
-			playSong("htG4.mp3");
-		}, 4000);
-		setTimeout(function(){
-			playSong("htA4.mp3");
-		}, 4500);
-		setTimeout(function(){
-			playSong("htG4.mp3");
-		}, 5000);
-		setTimeout(function(){
-			playSong("htC4.mp3");
-		}, 5500);
-		setTimeout(function(){
-			playSong("htB4.mp3");
-		}, 6000);
+		setTimeout(function(){playSong("htG4");}, 3500);
+		setTimeout(function(){playSong("htG4");}, 4000);
+		setTimeout(function(){playSong("htA4");}, 4500);
+		setTimeout(function(){playSong("htG4");}, 5000);
+		setTimeout(function(){playSong("htA4");}, 5500);
+		setTimeout(function(){playSong("htC4");}, 6000);
 		// pause
-		/*
-		setTimeout(function(){
-			playSong("htD4.mp3");
-		}, 4500);					
-		setTimeout(function(){
-			playSong("htE4.mp3");
-		}, 5000);
-		setTimeout(function(){
-			playSong("htC4.mp3");
-		}, 5500);
-		setTimeout(function(){
-			playSong("htC4.mp3");
-		}, 7000);*/				    
+		setTimeout(function(){playSong("htG4");}, 7000);
+		setTimeout(function(){playSong("htG4");}, 7500);
+		setTimeout(function(){playSong("htG4");}, 8000);
+		setTimeout(function(){playSong("htE4");}, 8500);
+		setTimeout(function(){playSong("htC4");}, 9000);
+		setTimeout(function(){playSong("htB4");}, 9500);
+		setTimeout(function(){playSong("htA4");}, 10000);
+		// pause
+		setTimeout(function(){playSong("htF4");}, 11000);
+		setTimeout(function(){playSong("htF4");}, 11500);
+		setTimeout(function(){playSong("htE4");}, 12000);
+		setTimeout(function(){playSong("htC4");}, 12500);
+		setTimeout(function(){playSong("htD4");}, 13000);
+		setTimeout(function(){playSong("htC4");}, 13500);
+		// pause
+	});
+
+	$(songtwo).click(function(){
+	// Happy Birthday Song
+	// timers were set to give the song a tru feel of how it actually sounds where 1000 = 1 second						
+		setTimeout(function(){playSong("htG4");}, 0);
+		setTimeout(function(){playSong("htC4");}, 500);
+		setTimeout(function(){playSong("htD4");}, 1000);
+		setTimeout(function(){playSong("htE4");}, 1500);
+		setTimeout(function(){playSong("htE4");}, 2500);
+		setTimeout(function(){playSong("htE4");}, 3500);
+		setTimeout(function(){playSong("htD4");}, 4000);
+		setTimeout(function(){playSong("htE4");}, 4500);
+		setTimeout(function(){playSong("htC4");}, 5500);
+		setTimeout(function(){playSong("htC4");}, 6500);
+		// pause
+		setTimeout(function(){playSong("htC4");}, 7500);
+		setTimeout(function(){playSong("htD4");}, 8000);
+		setTimeout(function(){playSong("htE4");}, 8500);
+		setTimeout(function(){playSong("htF4");}, 9000);
+		setTimeout(function(){playSong("htA4");}, 10000);
+		setTimeout(function(){playSong("htA4");}, 11000);
+		setTimeout(function(){playSong("htG4");}, 11500);
+		setTimeout(function(){playSong("htF4");}, 12000);
+		setTimeout(function(){playSong("htE4");}, 13000);
+		// pause
+		setTimeout(function(){playSong("htC4");}, 14000);
+		setTimeout(function(){playSong("htD4");}, 14500);
+		setTimeout(function(){playSong("htE4");}, 15000);
+		setTimeout(function(){playSong("htF4");}, 15500);
+		setTimeout(function(){playSong("htA4");}, 16500);
+		setTimeout(function(){playSong("htA4");}, 17500);
+		setTimeout(function(){playSong("htG4");}, 18000);
+		setTimeout(function(){playSong("htF4");}, 18500);
+		setTimeout(function(){playSong("htE4");}, 19500);
+		setTimeout(function(){playSong("htC4");}, 20500);
+		// pause
+		setTimeout(function(){playSong("htC4");}, 21500);
+		setTimeout(function(){playSong("htD4");}, 22000);
+		setTimeout(function(){playSong("htE4");}, 22500);
+		setTimeout(function(){playSong("htF4");}, 23500);
+		setTimeout(function(){playSong("htD4");}, 24000);
+		setTimeout(function(){playSong("htD4");}, 24500);
+		setTimeout(function(){playSong("htE4");}, 25500);
+		setTimeout(function(){playSong("htC4");}, 26000);
 	});
 });
